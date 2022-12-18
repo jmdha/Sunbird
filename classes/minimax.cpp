05b113061e8a91bef6536366d893632a4a8e6b64
jamadaha <jan.m.d.hansen@protonmail.com>
2022-12-18 20:12:05 +0100

    Check attacks before quiet

--- classes/minimax.cpp
+++ classes/minimax.cpp
@@ -35,19 +35,27 @@ int MiniMax::NegaMax(int depth, int alpha, int beta, U64 attackedSquares[2]) {
     int moveCount = moveGens[(int) board->GetColor()].GetAllMoves(&moves, *board, &attackSquares);
 
     int bestScore = -(int) PieceValue::Inf;
-    for (int i = 0; i < moveCount; i++) {
-        board->DoMove(&moves[i]);
-        int score = -NegaMax(depth - 1, -beta, -alpha, attackSquares);
-        board->UndoMove(moves[i]);
+    for (int moveType = 0; moveType < 2; moveType++) 
+        for (int i = 0; i < moveCount; i++) {
+            if (moves.at(i).IsCapture()) {
+                if (moveType == 1)
+                    continue;
+            } else {
+                if (moveType == 0)
+                    continue;
+            }
+            board->DoMove(&moves[i]);
+            int score = -NegaMax(depth - 1, -beta, -alpha, attackSquares);
+            board->UndoMove(moves[i]);
 
-        if (score >= beta)
-            return score;
-        if (score > bestScore) {
-            bestScore = score;
-            if (score > alpha)
-                alpha = score;
+            if (score >= beta)
+                return score;
+            if (score > bestScore) {
+                bestScore = score;
+                if (score > alpha)
+                    alpha = score;
+            }
         }
-    }
 
     return bestScore;
 }
