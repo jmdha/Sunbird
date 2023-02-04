be7ae68e4c0309becd6009b48392e39b5185b58d
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2023-02-04 17:14:38 +0100

    Adds fen engine

---
+++ builds/engine_fen.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include "board.hh"
+#include "board_importer.hh"
+#include "minimax.hh"
+
+int main(int argc, char* argv[]) {
+    BitShifts::Init();
+    Board board = Board();
+    BoardImporter::ImportFEN(&board, argv[2]);
+    MiniMax max = MiniMax(&board);
+    Move move = max.GetBestMove();
+    printf("%s\n", move.ToString().c_str());
+}
