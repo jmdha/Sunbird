03e5a51d965729b3d72b244130d44720f4659290
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2023-08-03 21:25:07 +0200

    refactor: reverts moves to internal int representation
    GCC could not figure out how to store multiple bitsets in a class.
    As such, a move with bitsets used 24 bytes.
    Now it uses 2 bytes.

--- chess/include/chess/internal/move.hpp
+++ chess/include/chess/internal/move.hpp
@@ -11,27 +11,27 @@ namespace Chess {
 class Move {
 public:
     Move() = default;
-    explicit Move(MoveType type) : _type((U8)type){};
+    explicit Move(uint16_t move) : move(move) {}
+    explicit Move(MoveType type) : move((U8)type & 0xf){};
 
-    Move(MoveType type, Square from, Square to) : _type((U8)type), _from((U8)from), _to((U8)to){};
+    Move(MoveType type, Square from, Square to)
+        : move(((U8)type & 0xf) + (((U8)from & 0x3f) << 4) + (((U8)to & 0x3f) << 10)){};
 
     inline std::string ToString() const;
-    inline MoveType GetType() const { return (MoveType)_type.to_ulong(); }
-    inline Square GetFrom() const { return (Square)_from.to_ulong(); }
-    inline Square GetTo() const { return (Square)_to.to_ulong(); }
-    inline bool IsCapture() const { return (_type.to_ulong() & CaptureBit) != 0; }
-    inline bool IsPromotion() const { return (_type.to_ulong() & PromotionBit) != 0; }
-    inline bool IsCastle() const { return (_type.to_ulong() & CastlingBit) != 0; }
-    inline bool IsEP() const { return (MoveType)_type.to_ulong() == MoveType::EPCapture; }
 
-    friend bool operator==(Move const &lhs, Move const &rhs) {
-        return lhs._type == rhs._type && lhs._from == rhs._from && lhs._to == rhs._to;
-    }
+    inline uint16_t GetValue() const noexcept { return move; }
+    inline MoveType GetType() const noexcept { return (MoveType)(move & 0xf); }
+    inline Square GetFrom() const noexcept { return (Square)((move >> 4) & 0x3f); }
+    inline Square GetTo() const noexcept { return (Square)((move >> 10) & 0x3f); }
+    inline bool IsCapture() const noexcept { return move & CaptureBit; }
+    inline bool IsPromotion() const noexcept { return move & PromotionBit; }
+    inline bool IsCastle() const noexcept { return move & CastlingBit; }
+    inline bool IsEP() const noexcept { return GetType() == MoveType::EPCapture; }
+
+    friend bool operator==(Move const &lhs, Move const &rhs) { return lhs.move == rhs.move; }
 
 private:
-    std::bitset<4> _type;
-    std::bitset<6> _from;
-    std::bitset<6> _to;
+    uint16_t move = 0;
 };
 
 inline std::string Move::ToString() const {
