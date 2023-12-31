c7e1cda737a9d09f9b98014f9ce9a6abc60b3dff
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2023-12-31 05:40:42 +0100

    feat: add bench

--- bin/uci.cpp
+++ bin/uci.cpp
@@ -1,6 +1,5 @@
 #include <climits>
 #include <iostream>
-#include <optional>
 #include <string>
 
 #include <chess/board.hpp>
@@ -8,9 +7,17 @@
 #include <engine/internal/tt.hpp>
 #include <engine/search.hpp>
 #include <unordered_map>
-#include <unordered_set>
 
 using namespace Chess;
+
+const std::vector<std::string> POSITIONS{
+    "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",                 // Init,
+    "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - ",        // Kiwipete
+    "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - ",                                   // NoCastle
+    "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1",         // Position 4
+    "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8  ",              // Position 5
+    "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10 " // Position 6
+};
 
 enum class Command { uci, isready, setoption, quit, position, ucinewgame, go, stop };
 
@@ -38,8 +45,24 @@ NEXT_TOKEN:
     return tokens;
 }
 
-int main() {
+int main(int argc, char **argv) {
     Engine::TT::Init(32);
+    if (argc > 1 && std::string(argv[1]) == "bench") {
+        size_t nodes = 0;
+        size_t time = 0;
+        for (const auto &pos : POSITIONS) {
+            Board board = Import::FEN(pos);
+
+            std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+            Engine::Search::GetBestMoveTime(board, 100);
+            std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+            nodes += board.MoveCount();
+            size_t t = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
+            time += t;
+        }
+        std::cout << nodes << " nodes " << nodes / time * 1000 << " nps" << '\n';
+        exit(0);
+    }
     Board board = Import::MoveSequence("");
     while (true) {
         std::string input;
