1daa022d41fc99c6dbbb4a90d5c291a2620f98da
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2024-01-03 03:17:00 +0100

    refactor: clean zobrist

--- chess/src/zobrist.cpp
+++ chess/src/zobrist.cpp
@@ -4,35 +4,31 @@
 
 using namespace Chess;
 
-namespace {
 constexpr int SQUARE_HASH_COUNT = 2 * 6 * 64;
-template <uint64_t l> constexpr std::array<uint64_t, l> GenerateHashes() {
-    std::array<uint64_t, l> tempTable;
+constexpr std::array<uint64_t, SQUARE_HASH_COUNT> HASHES = [] {
+    auto tempTable = decltype(HASHES){};
 
-    uint64_t startState = 0x181818ffff181818;
-    uint64_t lfsr = startState;
+    uint64_t lfsr = 0x181818ffff181818;
     uint64_t bit;
 
-    for (int i = 0; i < l; i++) {
+    for (int i = 0; i < SQUARE_HASH_COUNT; i++) {
         bit = ((lfsr >> 0) ^ (lfsr >> 2) ^ (lfsr >> 3) ^ (lfsr >> 5)) & 1u;
         lfsr = (lfsr >> 1) | (bit << 63);
         tempTable[i] = lfsr;
     }
 
     return tempTable;
-}
-constexpr std::array<uint64_t, SQUARE_HASH_COUNT> hashes = GenerateHashes<SQUARE_HASH_COUNT>();
-} // namespace
+}();
 
 uint64_t Zobrist::FlipSquare(uint64_t hash, Square square, PieceType type, Color color) {
-    return hash ^ hashes[(64 * 6) * (int)color + 64 * (int)type + (int)square];
+    return hash ^ HASHES[(64 * 6) * (int)color + 64 * (int)type + (int)square];
 }
 
 uint64_t Zobrist::FlipCastling(uint64_t hash, Color col, Castling side) {
-    return hash ^ hashes[SQUARE_HASH_COUNT + 2 * (int)col + (int)side];
+    return hash ^ HASHES[SQUARE_HASH_COUNT + 2 * (int)col + (int)side];
 }
 
 uint64_t Zobrist::FlipEnPassant(uint64_t hash, Column col) {
-    return hash ^ hashes[SQUARE_HASH_COUNT + 4 + Utilities::GetColumnIndex(col)];
+    return hash ^ HASHES[SQUARE_HASH_COUNT + 4 + Utilities::GetColumnIndex(col)];
 }
 uint64_t Zobrist::FlipColor(uint64_t hash) { return hash ^ 0xaa55aa55aa55aa55; }
